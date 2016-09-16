9affa5567025ff103e61148de654609f6c0033a8
Kaustubh Walokar <kaustubh.walokar@gmail.com>
2016-09-16 15:40:19 -0700

    mod java compatibility to 1.8

---
+++ src/main/java/client/Main.java
@@ -0,0 +1,10 @@
+package client;
+
+/**
+ * Created by kaustubh on 9/16/16.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Project Mithai...");
+    }
+}
