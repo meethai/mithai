41e9d86da1309b93b11711167995d9f0e94d8b78
Kaustubh Walokar <kaustubh.walokar@gmail.com>
2016-09-16 17:32:01 -0700

    scala main added

--- src/main/java/client/Main.java
+++ src/main/java/client/Main.java
@@ -6,5 +6,6 @@ package client;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Project Mithai...");
+
     }
 }
---
+++ src/main/scala/edu/sjsu/mithai/Main.scala
@@ -0,0 +1,10 @@
+package edu.sjsu.mithai
+
+/**
+  * Created by kaustubh on 9/16/16.
+  */
+object Main {
+  def main(args: Array[String]) {
+    print("Scala Ahoy!")
+  }
+}
