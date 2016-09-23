e40bcde7c2dabbb71fe19dcc9a2f845468c24c5a
saurabhjinturkar <saurabh.jinturkar@sjsu.edu>
2016-09-23 14:44:56 -0700

    Implement StoppableExecutableTask

--- src/main/java/edu/sjsu/mithai/util/StoppableRunnableTask.java
+++ src/main/java/edu/sjsu/mithai/util/StoppableRunnableTask.java
@@ -2,4 +2,15 @@ package edu.sjsu.mithai.util;
 
 public abstract class StoppableRunnableTask implements Runnable, Stoppable {
 
+    protected boolean stop;
+
+    public StoppableRunnableTask() {
+        this.stop = false;
+    }
+
+    @Override
+    public void stop() {
+        stop = true;
+    }
+
 }
