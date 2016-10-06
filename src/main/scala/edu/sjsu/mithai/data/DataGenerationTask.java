052a0ff35833c90e2a9476fbcdd359d2b38bafee
Kaustubh Walokar <kaustubh.walokar@gmail.com>
2016-10-05 22:41:06 -0700

    Added avro serializer

--- src/main/scala/edu/sjsu/mithai/data/DataGenerationTask.java
+++ src/main/scala/edu/sjsu/mithai/data/DataGenerationTask.java
@@ -2,26 +2,51 @@ package edu.sjsu.mithai.data;
 
 import edu.sjsu.mithai.config.Configuration;
 import edu.sjsu.mithai.config.MithaiProperties;
+import edu.sjsu.mithai.mqtt.MQTTPublisher;
 import edu.sjsu.mithai.sensors.IDevice;
 import edu.sjsu.mithai.util.StoppableExecutableTask;
+import org.apache.avro.generic.GenericData;
+import org.apache.avro.generic.GenericRecord;
+
+import java.io.IOException;
 
 public class DataGenerationTask extends StoppableExecutableTask {
 
     private SensorStore sensorStore;
     private Configuration configuration;
+    private AvroSerializationHelper avro;
+    private MQTTPublisher mqttPublisher;
+    private String topic;
 
-    public DataGenerationTask(Configuration configuration, SensorStore sensorStore) {
+    public DataGenerationTask(Configuration configuration, SensorStore sensorStore) throws IOException {
+
         this.configuration = configuration;
         this.sensorStore = sensorStore;
+        avro = new AvroSerializationHelper();
+        avro.loadSchema("sensor.json");
+        mqttPublisher = new MQTTPublisher(configuration.getProperty(MithaiProperties.MQTT_BROKER));
+        topic = configuration.getProperty(MithaiProperties.MQTT_TOPIC);
+
     }
 
     @Override
     public void execute() {
 
         for (IDevice device : sensorStore.getDevices()) {
-            // TODO send this data to MQTT using avro
+
             System.out.println(device.getId() + "=> " + device.sense());
+            GenericRecord record = new GenericData.Record(avro.getSchema());
+            record.put("id", device.getId());
+            record.put("value", device.sense());
+            try {
+                mqttPublisher.sendDataToTopic(avro.serialize(record), topic);
+                Thread.sleep(1 * 1000);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
         }
+
 
         try {
             Thread.sleep(Long.parseLong(configuration.getProperty(MithaiProperties.DATA_GENERATION_INTERVAL)));
