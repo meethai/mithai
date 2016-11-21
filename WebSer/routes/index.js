65308bd18d510859abe0215a2c8bde9a8a1532c0
Kaustubh Walokar <kaustubh.walokar@gmail.com>
2016-11-20 19:40:10 -0800

    added "/data" rest endpoint to server data allowing all origins

--- WebSer/routes/index.js
+++ WebSer/routes/index.js
@@ -2,6 +2,33 @@ var express = require('express');
 var cache = require('memory-cache');
 var router = express.Router();
 var x = "{ nodes:[ { id: 'node1', group: 0 },{ id: 'node2', group: 1 },{ id: 'node3', group: 2 },{ id: 'node5', group: 3 },{ id: 'sensor1', group: 0 },{ id: 'sensor2', group: 0 },{ id: 'sensor3', group: 0 } ],links:[ { source: 'node1', target: 'node2', value: 30 },{ source: 'node1', target: 'node3', value: 30 },{ source: 'node1', target: 'node5', value: 30 },{ source: 'node1', target: 'sensor1', value: 10 },{ source: 'sensor1', target: 'sensor2', value: 1 },{ source: 'sensor1', target: 'sensor3', value: 1 },{ source: 'node1', target: 'sensor2', value: 10 },{ source: 'sensor2', target: 'sensor1', value: 1 },{ source: 'sensor2', target: 'sensor3', value: 1 },{ source: 'node1', target: 'sensor3', value: 10 },{ source: 'sensor3', target: 'sensor1', value: 1 },{ source: 'sensor3', target: 'sensor2', value: 1 } ] }";
+var y = {
+    nodes: [{id: 'node1', group: 0}, {id: 'node2', group: 1}, {id: 'node3', group: 2}, {
+        id: 'node5',
+        group: 3
+    }, {id: 'sensor1', group: 0}, {id: 'sensor2', group: 0}, {id: 'sensor3', group: 0}],
+    links: [{source: 'node1', target: 'node2', value: 30}, {
+        source: 'node1',
+        target: 'node3',
+        value: 30
+    }, {source: 'node1', target: 'node5', value: 30}, {
+        source: 'node1',
+        target: 'sensor1',
+        value: 10
+    }, {source: 'sensor1', target: 'sensor2', value: 1}, {
+        source: 'sensor1',
+        target: 'sensor3',
+        value: 1
+    }, {source: 'node1', target: 'sensor2', value: 10}, {
+        source: 'sensor2',
+        target: 'sensor1',
+        value: 1
+    }, {source: 'sensor2', target: 'sensor3', value: 1}, {
+        source: 'node1',
+        target: 'sensor3',
+        value: 10
+    }, {source: 'sensor3', target: 'sensor1', value: 1}, {source: 'sensor3', target: 'sensor2', value: 1}]
+};
 /* GET home page. */
 router.get('/', function(req, res, next) {
     var data = cache.get('visualizationData');
@@ -18,6 +45,10 @@ router.get('/', function(req, res, next) {
 router.post('/ingress', function(req, res, next) {
     cache.put('visualizationData', req.body);
     res.send("Hello world!");
+});
+
+router.get('/data', function (req, res, next) {
+    res.send(JSON.stringify(y));
 });
 
 module.exports = router;
