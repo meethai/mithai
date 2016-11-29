c26f99435f49956b40df4dee220128baac7b3ae4
Kaustubh Walokar <kaustubh.walokar@gmail.com>
2016-11-29 12:58:14 -0800

    Change to accept new data format posted to /ingress

--- chrome_client/javascripts/parking.js
+++ chrome_client/javascripts/parking.js
@@ -3,17 +3,23 @@
  */
 
 $(document).ready(function () {
-    var x = 8;
-    var container = $('#row1');
-
-    for (i = 0; i < x; i++) {
-        container.append($('<div class="spot ver col-sm-1"></div>'));
-
-    }
-    var container2 = $('#row2');
-    for (i = 0; i < x; i++) {
-        container2.append($('<div class="spot green ver col-sm-1"></div>'));
-    }
+    // var x = 8;
+    // var container = $('#row1');
+    //
+    // for (i = 0; i < x; i++) {
+    //     // container.append($(
+    //         // '<div class="col-sm-1 spot-wrapper">' +
+    //         //     '<div class="spot ver col-sm-1 spot-wrapper"></div>'
+    //             // +
+    //             // '<div class="line"></div>' +
+    //         // '</div>'
+    //         // ));
+    //
+    // }
+    // var container2 = $('#row2');
+    // for (i = 0; i < x; i++) {
+    //     // container2.append($('<div class="spot ver col-sm-1 spot-wrapper green"></div>'));
+    // }
 
     $(".spot").on('click', function (e) {
         $(e.target).toggleClass("green");
