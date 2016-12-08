48e11dc7b8ca4e18b6177409a0e59e9f5a7e7e82
saurabhjinturkar <saurabh.jinturkar@sjsu.edu>
2016-12-07 16:27:49 -0800

    Add graphite exporter

--- src/main/java/edu/sjsu/mithai/apps/temperature/TemperatureHandler.java
+++ src/main/java/edu/sjsu/mithai/apps/temperature/TemperatureHandler.java
@@ -1,16 +1,38 @@
 package edu.sjsu.mithai.apps.temperature;
 
+import com.google.gson.Gson;
+import edu.sjsu.mithai.export.ExportMessage;
+import edu.sjsu.mithai.spark.Store;
 import edu.sjsu.mithai.util.Ihandler;
 
-/**
- * Created by sushained on 11/30/16.
- */
+import java.util.HashMap;
+import java.util.Map;
+
 public class TemperatureHandler implements Ihandler {
+
+    private Gson gson;
+    public TemperatureHandler() {
+        this.gson = new Gson();
+    }
 
     @Override
     public void handle(String functionName, String msg) {
-        if(functionName.equals("average")) {
+
+        if (functionName.equals("average")) {
             System.out.println("Average is =>" + msg);
+
+            Map<String, String> data = new HashMap<>();
+            data.put("key","temperature.average");
+            data.put("value", msg);
+            data.put("time", String.valueOf(System.currentTimeMillis()/1_000_000));
+
+            ExportMessage exportMessage = new ExportMessage(gson.toJson(data));
+
+            System.out.println("Data sending to Exporter: " + exportMessage.getMessage());
+            Store.messageStore().addMessage(exportMessage);
+
+        } else {
+            System.out.println(functionName + "=>" + msg);
         }
     }
 }
--- src/main/java/edu/sjsu/mithai/export/ExporterFactory.java
+++ src/main/java/edu/sjsu/mithai/export/ExporterFactory.java
@@ -3,11 +3,11 @@ package edu.sjsu.mithai.export;
 import edu.sjsu.mithai.config.Configuration;
 import edu.sjsu.mithai.config.MithaiProperties;
 import edu.sjsu.mithai.export.console.ConsoleExporter;
+import edu.sjsu.mithai.export.graphite.GraphiteExporter;
 import edu.sjsu.mithai.export.http.HttpExporter;
 import edu.sjsu.mithai.export.kafka.KafkaExporter;
 
-import static edu.sjsu.mithai.config.MithaiProperties.EXPORTER_KAFKA_TOPIC;
-import static edu.sjsu.mithai.config.MithaiProperties.EXPORTER_REMOTE_URI;
+import static edu.sjsu.mithai.config.MithaiProperties.*;
 
 public class ExporterFactory {
 
@@ -28,6 +28,10 @@ public class ExporterFactory {
 
             case "CONSOLE":
                 return new ConsoleExporter();
+
+            case "GRAPHITE":
+                return new GraphiteExporter(configuration.getProperty(GRAPHITE_HOSTNAME),
+                        Integer.parseInt(configuration.getProperty(GRAPHITE_PORT)));
 
             default:
                 return null;
